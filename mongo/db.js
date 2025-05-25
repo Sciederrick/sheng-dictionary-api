5edff346765b199327b5efbb0da708bd91471dc2
Sciederrick <derrickmbarani@gmail.com>
2025-05-25 19:58:56 +0300

    chore: modify string URI to match railway URI

--- mongo/db.js
+++ mongo/db.js
@@ -5,7 +5,7 @@ const db = {}
 
 db.init = async(callback) => {
   try {
-    const MONGO_URI = `mongodb+srv://${process.env.MONGO_UNAME}:${process.env.MONGO_PWD}@cluster0.yrrvwrm.mongodb.net/SHENG-API?retryWrites=true&w=majority`
+    const MONGO_URI = `${process.env.MONGO_URL}`
     await connect(MONGO_URI);
     console.log('\x1b[35m%s\x1b[0m', 'Status 200 => OK[mongoDB]');
     callback();
